792d400db8d51b32c6201b45b93d30fda4f7dc41
Daniela Vargas <danvargas94@hotmail.com>
2017-04-18 22:29:13 -0500

    Treshold function

--- percepton.py
+++ percepton.py
@@ -2,6 +2,20 @@ import fileinput
 from numpy import array, dot, random
 from random import randint
 
+def tresholdFunction(input):
+    dot = 0
+    for index, val in enumerate(input):
+        dot += val * w[index]
+    return 1 if dot > treshold else 0
+
+def getWeight(train):
+	output = train[1]
+	calculated = tresholdFunction(train[0])
+	error = output - calculated
+	for index, val in enumerate(w):
+		val += error * train[index]
+	return error
+
 training = []
 training_result = []
 test = []
@@ -30,6 +44,23 @@ print(training)
 print(training_result)
 print(test)
 
+w = random.rand(d)
+rounds = 100
+treshold = randint(0, 5);
+
+while (rounds > 0):
+    rounds -= 1
+    error = 0
+    for train in training:
+        error += pow(getWeight(train), 2)
+    if (error == 0):
+        break
+
+if error >= 1:
+	print("no solution found")
+else: 
+	for val in test:
+		print(tresholdFunction(val))
 
 
 
